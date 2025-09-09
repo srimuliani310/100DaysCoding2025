edb0ec7b2a5c351977c41786a32692689e7fdda3
srimuliani310 <srimeliani158@gmail.com>
2025-09-09 19:53:34 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,7 @@
+public class main
+  public static void main(String [] args) {
+    String nama = "Sri muliani";
+        
+        System.out.printf("%s", "HALO ");
+        System.out.print("Nama " );
+        System.out.println(" saya " + nama);
