39a4d3fb7f5031257ade04600cf90c0c03770658
srimuliani310 <srimeliani158@gmail.com>
2025-09-10 18:58:35 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // komentar ini untuk 1 baris
+        
+        /* komentar ini digunakan untuk beberapa kalimat atau lebih dari satu baris, seperti
+        baris pertama
+        baris kedua
+        dst...
+        */
+    }
+    
+}
