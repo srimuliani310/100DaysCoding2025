5e84f4ea1823eed1996a3460dc4a69917c420990
srimuliani310 <srimeliani158@gmail.com>
2025-09-12 19:36:47 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        // Data Numerik Bilangan Bulat
+        /*Tipe data : byte
+        Ukuran : 8 bit
+        Rentang Nilai :-128 s/d 127
+        */
+        // Contoh byte
+        byte umur = 18;
+        System.out.println("Umur\t\t: " + umur + "Tahun");
+        
+        /*Tipe data : short
+        Ukuran : 16 bit
+        Rentang Nilai :-32,728 s/d 32,767
+        */
+        // Contoh short
+        short jumlahPenduduk = 15000;
+        System.out.println("Jumlah Penduduk\t: " + jumlahPenduduk);
+        
+        /*Tipe data : int
+        Ukuran : 32 bit
+        Rentang Nilai :-2,147,483,648 s/d 2,147,483,647
+        */
+        // Contoh int
+        int gaji = 5000000;
+        System.out.println("Gaji\t\t: " + gaji);
+        
+        /*Tipe data : long
+        Ukuran : 64 bit
+        Rentang Nilai :-9,223,372,036,854,775,808 s/d 9,223,372,036,854,775,808
+        */
+        // Contoh long
+        long jarakBintang = 15000000000L; // Pakai 'L' di akhir
+        System.out.println("Jarak Bintang\t: " + jarakBintang);
+   
+    }  
+}
