5503f7bd8fc06bac1839ccd64d01333ecd9d303c
srimuliani310 <srimeliani158@gmail.com>
2025-09-13 08:43:29 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        float tinggiBadan = 150.3f;
+        float beratBadan = 45.5f;
+        System.out.println("Tinggi Badan : " + tinggiBadan);
+        System.out.println("Berat Badan : " + beratBadan);
+        
+        double pi = 3.141592653589793;
+        double angka = 12345.6789;
+        System.out.println("Nilai Pi = " + pi);
+        System.out.println("Nilai Angka = " + angka);
+   
+    }  
+}
