8a46d741c7c7913ac2c977e0bec5d2f788116fca
srimuliani310 <srimeliani158@gmail.com>
2025-09-14 10:51:03 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        char golonganDarah = 'A';
+        boolean javaSusah = true;
+        
+        System.out.println("Golongan Darah : " + golonganDarah);
+        System.out.println("Java itu susah : " + javaSusah);
+      
+    }  
+  }
