b7f5d128e4dff93d6a163a7e4299ca207e64c9d8
srimuliani310 <srimeliani158@gmail.com>
2025-09-15 15:58:46 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+package javaapplication12;
+public class Teks {
+    public static void main(String[] args) {
+        String nama = "Sri muliani";
+        String nim = "D0225310";
+        
+        System.out.println("Nama\t: " + nama);
+        System.out.println("NIM\t: " + nim);
+          
+    }
+          }
