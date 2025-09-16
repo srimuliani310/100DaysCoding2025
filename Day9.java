1546842dd4abab72f01fbcd3995b0b6a5d85ddf7
srimuliani310 <srimeliani158@gmail.com>
2025-09-16 18:59:35 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+package javaapplication12;
+public class Deklarasi {
+    public static void main(String[] args) {
+        // Deklarasi
+        int umur;
+        // Inisialisasi
+        umur = 18;
+        System.out.println("Umur : " + umur + " Tahun");
+        // Update Variabel      
+        umur = 20;
+        System.out.println("Umur : " + umur + " Tahun");
+          
+    }
+                           }
