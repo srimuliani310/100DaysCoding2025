6ca23f36bc356b9cc01f1ab0f3130d69c91bff08
srimuliani310 <srimeliani158@gmail.com>
2025-09-17 19:12:32 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       /* Constanta (Final Variabel) adalah Variabel yang
+        yang nilainya tetap (tidak bisa diubah lagi) setelah
+        didefinisikan.
+        */
+        final double phi = 3.14;
+        final int MAX = 100;
+        System.out.println("Nilai phi : " + phi);
+        System.out.println("Nilai Maksimal : " + MAX);
+ 
+    }
+    
+}
