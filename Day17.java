cf2380a77a60fc607d21cabf8b6d1bd9fb3d0e49
srimuliani310 <srimeliani158@gmail.com>
2025-09-24 20:52:01 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,10 @@
+package Day17;
+public class modulus {
+   public static void main(String[] args){
+       byte a = 25, b = 2;
+       
+       System.out.println("a = "+a);
+       System.out.println("b = "+b);
+       System.out.println("Sisa bagi = "+ (a%b));
+   } 
+}
