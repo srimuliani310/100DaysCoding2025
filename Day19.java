3770b255d0e8c53b16993105eb983089e230e7ad
srimuliani310 <srimeliani158@gmail.com>
2025-09-26 21:48:25 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String [] args) {
+        double angkaDouble = 45.34;
+        int angkaInt = (int) angkaDouble;
+        System.out.println("Double ke Int : "+angkaInt);
+        short angkaShort = 130;
+        byte angkaByte = (byte) angkaShort;
+        System.out.println("Short ke byte : "+angkaByte);
+        
+    }    
+}
