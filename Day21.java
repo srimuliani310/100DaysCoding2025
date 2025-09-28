c963917afc2cb23de4e57a16d1db5e0ccb49a540
srimuliani310 <srimeliani158@gmail.com>
2025-09-28 20:25:47 +0800

    Day21.java

--- Day21.java
+++ Day21.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String [] args) {
-        int v1 = 45;
-        int v2 = 5;
-        System.out.println("Sebelum :\n"+"v1 = "+v1+"\nv2 = "+v2);
-        v1 = v1+v2;
-        v2 = v1-v2;
-        v1 = v1-v2;
-        System.out.println("\nSetelah :\n"+"v1 = "+v1+"\nv2 = "+v2);
+        int a = 45, b = 5;
+        System.out.println("Sebelum :\n"+"a = "+a+"\nb = "+b);
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println("\nSetelah :\n"+"a = "+a+"\nb = "+b);
     }    
-          }
+}
