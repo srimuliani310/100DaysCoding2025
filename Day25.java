c76886fe101b467822c1e00a492cc147466122b0
srimuliani310 <srimeliani158@gmail.com>
2025-10-02 21:09:20 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,11 @@
+public class Day25 {
+    public static void main(String[] args){
+        byte a = 2;
+        ++a; // Increment (Ditambah 1)
+        System.out.println("Setelah Increment = " + a);
+        
+        --a; // Decrement (Dikurang 1)
+        System.out.println("Setelah Decrement = " + a);
+        
+    }
+}
