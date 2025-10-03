2da5992faedf4b2adbbca5e2585aba47356080a1
srimuliani310 <srimeliani158@gmail.com>
2025-10-03 20:34:33 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,19 @@
+package Day;
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 2;
+        System.out.println(a);
+        a -= 2;
+        System.out.println(a);
+        a *= 2;
+        System.out.println(a);
+        a /= 2;
+        System.out.println(a);
+        a %= 2;
+        System.out.println(a);
+        
+        
+        
+    }
+}
