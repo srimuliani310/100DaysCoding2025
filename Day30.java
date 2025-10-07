0710ab5ab22b1c7aed9b59d70a198ac6a23b265c
srimuliani310 <srimeliani158@gmail.com>
2025-10-07 22:19:42 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,8 @@
+public class c2 {
+    public static void main(String[] args) {
+        byte a = 1, b =2;
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+                
+    }
+}
