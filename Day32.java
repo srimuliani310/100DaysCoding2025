0162559482fa9f36d8299648087e608ca8bd42bd
srimuliani310 <srimeliani158@gmail.com>
2025-10-09 20:26:37 +0800

    Day32.java

--- Day32.java
+++ Day32.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 public class Day32 {
     public static void main(String[] args) {
-        String userB = "admin";
-        String passB = "12345";
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan username: ");
-        String usr = sc.nextLine();
-        System.out.print("Masukkan password: ");
-        String pass = sc.nextLine();
+        System.out.print("Masukkan nilai UTS: ");
+        int uts = sc.nextInt();
+        System.out.print("Masukkan nilai UAS: ");
+        int uas = sc.nextInt();
         
-        if(!usr.equalsIgnoreCase(userB) || !pass.equalsIgnoreCase(passB)){
-            System.out.println("Login gagal");
+        if(uts<60 || uas<60){
+            System.out.println("Anda boleh mengikuti ujian susulan");
         }else{
-            System.out.println("Login berhasil");
+            System.out.println("Anda tidak perlu mengikuti ujian susulan");
         }
     }
 }
