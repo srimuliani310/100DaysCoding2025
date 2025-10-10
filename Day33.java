1c13e85c0520adf7876c9c6e289e635e199e9615
srimuliani310 <srimeliani158@gmail.com>
2025-10-10 21:12:35 +0800

    Day33.java

--- Day33.java
+++ Day33.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
+        int a = 1, b = 2;
         System.out.println(!(a==b));
         
     }
