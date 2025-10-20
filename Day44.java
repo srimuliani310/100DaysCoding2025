47649a4a916ba3c4bd7d6b13b12348eb454cd47d
srimuliani310 <srimeliani158@gmail.com>
2025-10-21 00:08:15 +0800

    Day44.java

---
+++ Day44.java
@@ -0,0 +1,21 @@
+package latihan;
+import java.util.Scanner;
+public class Day44 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nilai: ");
+        int nilai = sc.nextInt();
+        if (nilai >= 90) {
+            System.out.println("Predikat A");
+            if (nilai == 100) {
+                System.out.println("Sempurna");
+            }
+        } else if (nilai >= 80 && nilai <= 89) {
+            System.out.println("Predikat B");
+        } else if (nilai >= 70 && nilai <= 79) {
+            System.out.println("Predikat C");
+        } else {
+            System.out.println("Tidak lulus");
+        }
+    }
+}
