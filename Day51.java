0e3146bd01689196dd16f52eee65f0607c698f2f
srimuliani310 <srimeliani158@gmail.com>
2025-10-28 22:49:54 +0800

    Day51.java

---
+++ Day51.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Day51 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("angka: ");
+        int angka = sc.nextInt();
+        int i = 1;
+        while(i<=angka){
+            System.out.print(i+" ");
+            i++;
+        }
+        System.out.println("");
+        
+    }
+}
