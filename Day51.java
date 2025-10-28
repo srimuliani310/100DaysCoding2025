df0181700554ec04a6813de0b20214f994e4d25a
srimuliani310 <srimeliani158@gmail.com>
2025-10-28 23:00:01 +0800

    Day51.java

---
+++ Day51.java
@@ -0,0 +1,9 @@
+public class Day51 {
+    public static void main(String[] args) {
+        int batas = 5;
+        for (int i = 1; i <= batas; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+    }
+}
