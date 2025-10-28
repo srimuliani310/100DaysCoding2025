e37fd5affb89b032ea2222da64225254582d4895
srimuliani310 <srimeliani158@gmail.com>
2025-10-28 22:57:01 +0800

    Day52.java

--- Day52.java
+++ Day52.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Day51 {
+public class Day52 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("angka: ");
