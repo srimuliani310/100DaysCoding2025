500a06e968132dc59a73b9a0db4dc27fc97f60d4
srimuliani310 <srimeliani158@gmail.com>
2025-11-04 17:43:00 +0800

    Day58.java

---
+++ Day58.java
@@ -0,0 +1,11 @@
+public class Day58 {
+    public static void main(String[] args){
+        for(int i = 1 ; i<=5 ; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+        for(int i = 5 ; i>=1; i--){
+            System.out.print(i+" ");
+        }
+}
+}
