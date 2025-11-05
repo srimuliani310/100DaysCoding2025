36708919b779ebab83674bcde1349025bb5bd9ad
srimuliani310 <srimeliani158@gmail.com>
2025-11-05 19:28:31 +0800

    Day59.java

---
+++ Day59.java
@@ -0,0 +1,17 @@
+public class Day59 {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println("");
+        for (int i = 1; i <= n; i++) {
+            if(i%2!=0){
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println("");
+    }
+}
