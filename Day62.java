c9ab26b87f37f3e3292b053da2c45dae3d009368
srimuliani310 <srimeliani158@gmail.com>
2025-11-08 18:57:57 +0800

    Day62.java

---
+++ Day62.java
@@ -0,0 +1,17 @@
+package lathiancoding;
+import java.util.Scanner;
+public class Day62 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int hasil = 0;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i);
+            if(i<n){
+                System.out.print("x");
+            }
+            hasil+=i;
+        }
+        System.out.println(" = "+hasil);
+    }
+}
