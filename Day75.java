21a4720c9e6c8b888562b1b63ebc287bf66da66d
srimuliani310 <srimeliani158@gmail.com>
2025-11-21 21:25:32 +0800

    Add Day75.java for string manipulation example

---
+++ Day75.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Day75 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String kalimat = "Hello world!";
+        
+        System.out.println("Kalimat: "+kalimat);
+        System.out.println("Panjang Kalimat: "+kalimat.length());
+        System.out.println("Huruf besar: "+kalimat.toUpperCase());
+        System.out.println("Huruf kecil: "+kalimat.toLowerCase());
+    }
+}
