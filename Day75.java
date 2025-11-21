640b9319ae029da21afd972676da9ee902ade983
srimuliani310 <srimeliani158@gmail.com>
2025-11-21 21:26:46 +0800

    Day75.java

--- Day75.java
+++ Day75.java
@@ -3,7 +3,7 @@ public class Day75 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String kalimat = "Hello world!";
-        
+    
         System.out.println("Kalimat: "+kalimat);
         System.out.println("Panjang Kalimat: "+kalimat.length());
         System.out.println("Huruf besar: "+kalimat.toUpperCase());
