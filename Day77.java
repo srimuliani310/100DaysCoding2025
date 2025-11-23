e066fe065a153ee4da34807d02013cc7f0865296
srimuliani310 <srimeliani158@gmail.com>
2025-11-23 19:59:51 +0800

    Day77.java

---
+++ Day77.java
@@ -0,0 +1,10 @@
+public class Day77 {
+    public static void main(String []args){
+        String t1 = "Hello";
+        System.out.println(t1.substring(1,4));
+        String t2 = "abc";
+        System.out.println(t2.replace("a","x"));
+        String t3 = " abc ";
+        System.out.println(t3.trim());
+    }
+}
