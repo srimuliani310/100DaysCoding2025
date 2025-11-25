3d3df2f0d8a9127af8495558b10510c1de262564
srimuliani310 <srimeliani158@gmail.com>
2025-11-25 23:24:01 +0800

    Day79.java

--- Day79.java
+++ Day79.java
@@ -1,24 +1,23 @@
-
 import java.util.Scanner;
-
 public class Day79 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String uB = "admin";
         String pB = "admin123";
-        boolean ditemukan = false;
-        while(!ditemukan){
+        boolean cek = false;
+        while(!cek){
             System.out.print("Username: ");
             String user = sc.nextLine();
             System.out.print("Password: ");
             String pass = sc.nextLine();
             if(user.equalsIgnoreCase(uB) && pass.equalsIgnoreCase(pB)){
-                ditemukan = true;
+                cek = true;
                 System.out.println("Login berhasil!");
                 break;
             }else{
             System.out.println("Login gagal!, coba lagi");
         }
         }
+       
     }
 }
