c50387c7944eb42f77ea4e18aabb2201761691fe
srimuliani310 <srimeliani158@gmail.com>
2025-11-26 21:30:45 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,9 @@
+public class Day80 {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        for (int i : a) {
+            System.out.print(i+" ");
+        }
+        System.out.println("");
+    }
+}
