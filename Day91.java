75923f0415ac28141c8044f467a5724caecf7075
srimuliani310 <srimeliani158@gmail.com>
2025-12-07 23:19:42 +0800

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Day91 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int umur = sc.nextInt();
+        Umur(umur);
+        
+    }
+    static void Umur(int umur){
+        System.out.println("Umur saya: "+umur);
+    }
+}
