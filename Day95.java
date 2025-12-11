a7defbbbc1fb2337a39c72897f4e80c9ab177c2e
srimuliani310 <srimeliani158@gmail.com>
2025-12-11 20:10:01 +0800

    Day95

---
+++ Day95.java
@@ -0,0 +1,9 @@
+public class Day95 {
+    public static void main(String[] args){
+        int u = 18;
+        umur(u);
+    }
+    public static void umur(int u){
+        System.out.println(u+" tahun");
+    }
+}
