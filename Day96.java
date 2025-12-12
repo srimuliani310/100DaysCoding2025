4a7a2a107ef2c1087ac657b29b8ef578322c5417
srimuliani310 <srimeliani158@gmail.com>
2025-12-12 20:34:09 +0800

    Day96

--- Day96.java
+++ Day96.java
@@ -1,18 +1,28 @@
+package LatihanMengulangSoal;
 import java.util.Scanner;
 public class Day96 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan a: ");
         int a = sc.nextInt();
         System.out.print("Masukkan b: ");
         int b = sc.nextInt();
-        hasil(a,b);
+        System.out.println("Hasil penjumlahan: "+penjumlahan(a,b));
+        System.out.println("Hasil pengurangan: "+pengurangan(a,b));
+        System.out.println("Hasil perkalian: "+perkalian(a,b));
+        System.out.println("Hasil pembagian: "+pembagian(a,b));
     }
-    public static void hasil(int a, int b){
-        System.out.println("Hasil penjumlahan a+b = "+(a+b));
-        System.out.println("Hasil pengurangan a-b = "+(a-b));
-        System.out.println("Hasil perkalian a*b = "+(a*b));
-        System.out.println("Hasil pembagian a/b = "+(a/b));
-   
-    }
+    public static int penjumlahan(int a, int b){
+        return a+b;
+    }  
+    public static int pengurangan(int a, int b){
+        return a-b;
+    }  
+    public static int perkalian(int a, int b){
+        return a*b;
+    }  
+    public static int pembagian(int a, int b){
+        return a/b;
+    }  
+    
 }
